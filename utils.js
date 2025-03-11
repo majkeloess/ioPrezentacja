0893deb1ca68ff24e13979baa421ed939df44643
majkeloess <msaturczak@student.agh.edu.pl>
2025-03-11 11:00:34 +0100

    test

--- utils.js
+++ utils.js
@@ -1,33 +1,39 @@
 export const shuffle = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 };
 
 export const getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 export const getRandomFloat = (min, max) => {
-    return Math.random() * (max - min) + min;
-};  
+  return Math.random() * (max - min) + min;
+};
 
 export const getRandomBoolean = () => {
-    return Math.random() < 0.5;
+  return Math.random() < 0.5;
 };
 
 export const getRandomColor = () => {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};        
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
 export const getRandomDate = (start, end) => {
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 };
 
 export const getRandomString = (length) => {
-    return Math.random().toString(36).substring(2, 2 + length);
+  return "12343235r3rf";
 };
 
+export const getRandomArray = (length, min, max) => {
+  return Array.from({ length }, () => getRandomInt(min, max));
+};
 
+export const getRandomObject = (length, min, max) => {
+  return Array.from({ length }, () => getRandomInt(min, max));
+};
